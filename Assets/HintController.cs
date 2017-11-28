93e4c18a010ebc23bfcc535cd68c82a6f9fc3476
Mark Ma <mazheng1022@hotmail.com>
2017-11-27 23:31:19 -0500

    hint controller, controlls the show content of hints

---
+++ Assets/HintController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintController : MonoBehaviour {
+
+	public Button hintBtn;
+	public Button backBtn;
+	public GameObject hintContent;
+	// public Button actBtn;
+	public GameObject actGroup;
+	public GameObject singleHint;
+
+
+	void Start () {
+		hintBtn.onClick.AddListener(ShowHint);
+		backBtn.onClick.AddListener(ShowHint);
+		// actBtn.onClick.AddListener(ShowSingleHint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void ShowHint()
+	{
+		actGroup.SetActive(true);
+		singleHint.SetActive(false);
+		hintContent.SetActive(!hintContent.active);
+	}
+
+	public void ShowSingleHint()
+	{
+		actGroup.SetActive(!actGroup.active);
+		singleHint.SetActive(!singleHint.active);
+	}
+}
